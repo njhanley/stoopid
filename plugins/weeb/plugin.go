2585d5f98a7c56f8b64657e67194a2a24cb436e7
njhanley <njhanley@users.noreply.github.com>
2018-02-14 04:08:52 -0500

    add cooldown to avoid spam

--- plugins/weeb/plugin.go
+++ plugins/weeb/plugin.go
@@ -2,6 +2,8 @@ package weeb
 
 import (
 	"log"
+	"sync"
+	"time"
 	"unicode"
 
 	dg "github.com/bwmarrin/discordgo"
@@ -13,19 +15,39 @@ func Plugin() bot.Plugin {
 }
 
 var plugin = bot.SimplePlugin("weeb", func(b *bot.Bot) error {
+	lastCallout = make(map[string]time.Time)
 	b.Session.AddHandler(handle)
 	return nil
 })
 
+func containsJapanese(s string) bool {
+	for _, r := range s {
+		if unicode.In(r, unicode.Han, unicode.Hiragana, unicode.Katakana) {
+			return true
+		}
+	}
+	return false
+}
+
+var (
+	lastCallout map[string]time.Time
+	mutex       sync.Mutex
+)
+
 func handle(s *dg.Session, mc *dg.MessageCreate) {
 	m := mc.Message
-	for _, r := range m.Content {
-		if unicode.In(r, unicode.Han, unicode.Hiragana, unicode.Katakana) {
-			_, err := s.ChannelMessageSend(m.ChannelID, m.Author.Mention()+" is a filthy WEEB!")
-			if err != nil {
-				log.Print(err)
-			}
-			return
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	if time.Since(lastCallout[m.Author.ID]) < time.Minute {
+		return
+	}
+
+	if containsJapanese(m.Content) {
+		lastCallout[m.Author.ID] = time.Now()
+		_, err := s.ChannelMessageSend(m.ChannelID, m.Author.Mention()+" is a filthy WEEB!")
+		if err != nil {
+			log.Print(err)
 		}
 	}
 }
